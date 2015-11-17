b178928f016c817e5967f3f3d7e0b75415743a4e
Yuri <yurishafet@gmail.com>
2015-11-17 22:01:51 +0200

    started first question

---
+++ ass1/q1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from scapy.all import *
+import argparse
+
+def analyze_tcp_opts():
+    pass
+
+def analyze_window_size():
+    pass
+
+def analyze_ttl():
+    pass
+
+if __name__ == "__main__":
+    pass
