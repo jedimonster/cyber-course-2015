7fa14aee9156abcb84b806a22124b59df12ac1bf
jedimonster <itay.itay@gmail.com>
2015-12-04 04:34:19 -0800

    q4

--- ass1/q4.py
+++ ass1/q4.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
 import argparse
-
 from scapy.all import *
 
 # all networks that aren't internet, in readable format.
-# turn it into integers to be used as masks.
+# we turn it into integers to be used as masks.
 RESERVED_NETWORKS = [('0.0.0.0', 8),
                      ('10.0.0.0', 8),
                      ('100.64.0.0', 10),
@@ -25,6 +24,11 @@ RESERVED_NETWORKS = [('0.0.0.0', 8),
 
 
 def ip_to_binary(ip):
+    """
+    turns a string IP into a 32bit integer.
+    :param ip: string ip, assumed to be correct.
+    :return: 32bit unsigned integer
+    """
     result = 0
     ip = ip.split(".")
     if len(ip) < 4:
@@ -38,14 +42,31 @@ def ip_to_binary(ip):
 
 
 def ip_in_network(ip, network):
+    """
+    checks if the given ip belongs to the given network mask
+    :param ip: integer ip
+    :param network: (integer subnet mask, integer significant bits)
+    :return: True iff ip is in network
+    """
     return mask_ip(ip, network[1]) == network[0]
 
 
 def mask_ip(ip, bits):
+    """
+    uses bitwise operators to mask an ip to its first bits bits.
+    :param ip: integer ip
+    :param bits: bits to retain
+    :return: integer ip masked to bits bits
+    """
     return ip & ((~0) << (32 - bits))
 
 
 def filter_external_ip(packet):
+    """
+    checks if the given packet is destined to a reserved ip address, or the internet
+    :param packet: scapy packet
+    :return: True iff packet is destined to the internet
+    """
     if IP in packet:
         dst_ip = ip_to_binary(packet[IP].dst)
         for network in RESERVED_MASKS:
@@ -57,7 +78,12 @@ def filter_external_ip(packet):
 
 def detect_gateways(packets):
     # first detect packets that are destined outside, their destination MACs are gateways
-    outgoing_packets = packets.filter(filter_external_ip)  # why doesn't python have .partition?
+    """
+    Detects default gateways by looking for packetes that are destined to the internet, but sent to a local device
+    :param packets: list of scapy packets
+    :return: default gateways, represented as [(mac, ip), ...]
+    """
+    outgoing_packets = packets.filter(filter_external_ip)
     gateways_macs = set([packet.dst for packet in outgoing_packets])
 
     # build mac->ip dict from ARP requests/response. we assume no-one lies.
@@ -73,7 +99,7 @@ def detect_gateways(packets):
 RESERVED_MASKS = [(ip_to_binary(x[0]), x[1]) for x in RESERVED_NETWORKS]
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Send HTTP Request')
+    parser = argparse.ArgumentParser(description='Detect default gateway in PCAP file')
     parser.add_argument('file', type=str, nargs=1,
                         help='PCAP file')
     args = parser.parse_args()
