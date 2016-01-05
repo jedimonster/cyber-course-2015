abcb0bba9f2a5091d54d243e8406f726ea3fe704
jedimonster <itay.itay@gmail.com>
2016-01-05 09:34:24 -0800

    q2 client now hashes path

--- ass2/q2_client.py
+++ ass2/q2_client.py
@@ -9,18 +9,18 @@ from scapy.tools.UTscapy import sha1
 from q1 import SpoofedTCPIPConnection
 
 if __name__ == '__main__':
-    src_ip = '192.168.1.1'
+    src_ip = '192.168.1.17'
     src_port = random.randint(1024, 65535)
     dst_ip = '192.168.1.2'
     dst_port = 8080
     c = SpoofedTCPIPConnection(src_ip, src_port, dst_ip, dst_port)
+    path = '/'
 
     c.connect()
     with codecs.open("client_secret", encoding='utf-8') as f:
         client_secret = f.read().replace('\n', "")
         current_time = int(time.time()) << 3
-        secret = sha1(str(client_secret) + str(current_time))
-        print current_time
-        c.send_data('GET / HTTP/1.0\r\n' +
+        secret = sha1(str(client_secret) + str(current_time) + str(path))
+        c.send_data('GET ' + path + ' HTTP/1.0\r\n' +
                     'secret: ' + secret + '\r\n' +
                     '\r\n')
