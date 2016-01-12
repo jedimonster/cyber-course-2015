6c8a9b57838db3d78df0ef17a2be60f6b8166334
jedimonster <itay.itay@gmail.com>
2016-01-12 09:35:30 -0800

    Q3 reversed order, timer, general good stuff

--- ass2/q3.py
+++ ass2/q3.py
@@ -1,11 +1,13 @@
 import argparse
 import json
-from scapy.all import *
 from netfilterqueue import NetfilterQueue
+from threading import Timer
 
+from scapy.all import *
 from scapy.tools.UTscapy import sha1
 
 REG_HTTP_GET = re.compile("^GET\s([A-Za-z0-9\/\-\._~:\?\\#]+)\sHTTP\/\d\.\d")
+IP_PROTOCOLS_TCP = 6
 
 
 class HttpInspector:
@@ -43,7 +45,7 @@ class HttpInspector:
 class IPSniffer(object):
     def __init__(self, inspector):
         self.inspector = inspector
-        self._sesssion_fragments_dict = dict()
+        self._sesssion_fragments_dict = defaultdict(list)
 
     def process_packet(self, pkt):
         """
@@ -53,41 +55,58 @@ class IPSniffer(object):
         scapy_packet = IP(pkt.get_payload())
 
         if IP in scapy_packet:
-
             # check more fragments flag
             sess = (scapy_packet.src, scapy_packet.dst, scapy_packet[IP].id)
-            if scapy_packet[IP].flags & 1 > 0:  # there are more fragments
-                pkt.accept()  # we'll drop the last fragment if it's malicious.
-                if sess in self._sesssion_fragments_dict:
-                    self._sesssion_fragments_dict[sess].append(scapy_packet)
+            session_fragments = self._sesssion_fragments_dict[sess]
+            session_fragments.append(scapy_packet)
+            # remove the packet from our cache in X seconds:
+            Timer(30, lambda: session_fragments.remove(scapy_packet))
+            # TODO clear empty key #########################@@@@@@@@@@@@@@@^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+            full_packet = self._reconstruct_fragments(sess)
+
+            if full_packet is None:
+                # packet is a fragment of a bigger packet, we accept it,
+                # and we'll reject, if needed, when we can reconstruct the entire thing.
+                pkt.accept()
+            else:
+                # this is either a stand alone packet, a reconstructed packet,
+                # or the last fragment of a fragmented packet with offset = 0
+                # in all those cases, we inspect and act accordingly.
+                if self.inspector.inspect(full_packet):
+                    pkt.accept()
                 else:
-                    self._sesssion_fragments_dict[sess] = [scapy_packet]
-            else:  # no more fragments
-                if sess not in self._sesssion_fragments_dict:
-                    if self.inspector.inspect(scapy_packet[IP]):
-                        pkt.accept()
-                    else:
-                        pkt.drop()
-                # need to reconstruct the packet
-                else:
-                    self._sesssion_fragments_dict[sess].append(scapy_packet)
-                    reconstructed_pkt = self._reconstruct_fragments(self._sesssion_fragments_dict[sess])
-                    if self.inspector.inspect(reconstructed_pkt):
-                        pkt.accept()
-                    else:
-                        pkt.drop()
-                    self._sesssion_fragments_dict[sess] = None
+                    print "dropped for one reason or another"
+                    pkt.drop()
+
         else:
             pkt.accept()
 
-    def _reconstruct_fragments(self, scapy_packets):
-        # scapy_packets = [IP(x.get_payload()) for x in pkts]
-        # sort by offset
+    def _reconstruct_fragments(self, sess):
+        """
+        attempts to reconstruct the packets in sess,
+        return entire packet if successful (i.e. we have all the parts), None otherwise.
+        :param sess:
+        """
+        pkts = sorted(self._sesssion_fragments_dict[sess],
+                      key=lambda p: p[IP].frag)
+        if pkts[-1][IP].flags & 1 > 0:
+            return None
+        if pkts[0][IP].frag > 0:
+            return None
 
-        sorted_packets = sorted(scapy_packets, key=lambda x: x[IP].frag)
-        payloads = "".join([str(x.payload) for x in sorted_packets])
-        reconstructed_packet = IP(sorted_packets[-1][IP].build()[:20]) / TCP(payloads)
-        print reconstructed_packet.show()
+        next_offset = 0
+        for pkt in pkts:
+            if next_offset != pkt[IP].frag:
+                return None
+            next_offset += len(pkt.payload)
+
+        payloads = "".join([str(x.payload) for x in pkts])
+        if pkts[-1][IP].proto == IP_PROTOCOLS_TCP:
+            payloads = TCP(payloads) # we don't current support UDP inspections.
+
+        reconstructed_packet = IP(pkts[-1][IP].build()[:20]) / payloads
+
         return reconstructed_packet
 
 
@@ -117,7 +136,8 @@ class SSHInspector(object):
                 try:
                     client_secret = self._get_client_secret(client_ip)
                 except KeyError:
-                    import pdb;pdb.set_trace()
+                    import pdb;
+                    pdb.set_trace()
                     print "KeyError"
                     return False
 
@@ -131,14 +151,13 @@ class SSHInspector(object):
                     elif scapy_packet[TCP].flags == 1:
                         print "Rejecting SSH from %s" % (client_ip)
                         self._allowed_connections.remove(session)
-
-
+                return True
             # SSH - is he authorized?
             elif scapy_packet[TCP].dport == 22 or scapy_packet[TCP].sport == 22:
-                print '*'*60
-                print session
-                print self._allowed_connections
-                print '&'*60
+                # print '*'*60
+                # print session
+                # print self._allowed_connections
+                # print '&'*60
                 return session in self._allowed_connections
 
     def _get_client_secret(self, ip):
@@ -173,7 +192,7 @@ if __name__ == '__main__':
     http_inspector = HttpInspector(filtered_extensions, silent)
     ssh_inspector = SSHInspector()
     multi_inspector = ChainedInspector(ssh_inspector, http_inspector)
-    sniffer = IPSniffer(multi_inspector)
+    sniffer = IPSniffer(http_inspector)
     nfqueue = NetfilterQueue()
 
     try:
